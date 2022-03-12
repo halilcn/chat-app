272e9d7609ae75b3b9b148c9156fa9a9e5578b59
halil <halilcan@Halil-MacBook-Pro.local>
2022-03-12 20:03:23 +0300

    update files andd packages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,9 @@
 import './pre-start'; // Must be the first import
-import logger from 'jet-logger';
+
 import server from './server';
 
+const port = (process.env.PORT || 3000);
 
-// Constants
-const serverStartMsg = 'Express server started on port: ',
-        port = (process.env.PORT || 3000);
-
-// Start server
 server.listen(port, () => {
-    logger.info(serverStartMsg + port);
+    console.info('Express server started on port: ' + port);
 });
---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,10 @@
+import logger from 'morgan';
+import helmet from 'helmet';
+import express from 'express';
+
+module.exports = (app: any) => {
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(helmet());
+};

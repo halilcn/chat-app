16008a3463a98445ee9a893578508b64959171b2
halil <halilcan@Halil-MacBook-Pro.local>
2022-03-13 19:22:10 +0300

    fixed nodemon

--- backend/src/controllers/user/user-controller.ts
+++ backend/src/controllers/user/user-controller.ts
@@ -6,7 +6,7 @@ import { TestError } from '@shared/errors';
 const index: RequestHandler = (req, res, next) => {
   // eslint-disable-next-line @typescript-eslint/require-await
   handle(async () => {
-    res.send('aaasda');
+    res.send('asdsa');
 
     throw new TestError();
   }, next);

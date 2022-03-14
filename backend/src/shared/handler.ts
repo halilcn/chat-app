fdf9d70d9a45bd9b69e7dff7a0736ac192c70585
halil <halilcan@Halil-MacBook-Pro.local>
2022-03-14 16:46:32 +0300

    update handler

--- backend/src/shared/handler.ts
+++ backend/src/shared/handler.ts
@@ -3,19 +3,20 @@ import { NextFunction, Request, Response } from 'express';
 import response from '@shared/response';
 import CustomError from '@shared/errors/custom-error';
 
-interface IProcessFunction {
+interface IRouterParams {
   (req: Request, res: Response, next: NextFunction): Promise<void>;
 }
 
-//todo:types
-const handler =
-  (processFunction: IProcessFunction) =>
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      await processFunction(req, res, next);
-    } catch (err) {
-      err instanceof CustomError ? next(response.error(err.message, err.code)) : next(response.error());
-    }
-  };
+interface IHandler {
+  (processFunction: IRouterParams): IRouterParams;
+}
+
+const handler: IHandler = processFunction => async (req, res, next) => {
+  try {
+    await processFunction(req, res, next);
+  } catch (err) {
+    err instanceof CustomError ? next(response.error(err.message, err.code)) : next(response.error());
+  }
+};
 
 export default handler;

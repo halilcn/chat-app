94a4bc2de5a5ad97941362b257e2feb751720206
halilcn <halilc.2001@gmail.com>
2022-03-16 15:57:11 +0300

    updated middleware  validate

--- backend/src/shared/validate-middleware/index.ts
+++ backend/src/shared/validate-middleware/index.ts
@@ -5,16 +5,17 @@ import response from '@shared/response';
 
 export default (schema: ObjectSchema) => (req: Request, res: Response, next: NextFunction) => {
   const { error } = schema.validate(req.body, { abortEarly: false });
+
   if (error) {
-    const test: Array<object> = error?.details.map((error: object) => {
+    const processedErrors = error?.details.map(error => {
       return {
         message: error.message,
         context: error.context
       };
     });
 
-   next(response.invalidInput(test))
+    next(response.invalidInput(processedErrors));
   }
 
-    next();
+  next();
 };

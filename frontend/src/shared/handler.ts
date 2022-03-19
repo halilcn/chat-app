b9d63f5fa475e2d5d0c56f60a8a36722ae3a47f3
halilcn <halilc.2001@gmail.com>
2022-03-19 11:59:49 +0300

    added handler method

---
+++ frontend/src/shared/handler.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosError } from 'axios';
+
+interface IHandler {
+  (handle: () => Promise<void>, customCatch: (err: AxiosError) => Promise<boolean>): Promise<void>;
+}
+
+const handler: IHandler = async (
+  handle,
+  customCatch = async () => {
+    return false;
+  }
+) => {
+  try {
+    await handle();
+  } catch (err: unknown | AxiosError) {
+    if (axios.isAxiosError(err) && err?.response && (await customCatch(err))) return;
+    alert('Hata meydana geldi!');
+  }
+};
+
+export default handler;

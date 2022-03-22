6f366bfd0d2bb64d9617903899a5a7e4f085b819
halilcn <halilc.2001@gmail.com>
2022-03-22 21:39:32 +0300

    updated auth middleware

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -2,32 +2,27 @@ import jwt from 'jsonwebtoken';
 
 import handler from '@shared/handler';
 import User from '@models/user-model';
-import response from '@shared/response';
 import { AuthenticationError } from '@shared/errors';
-import Logger from '@shared/logger';
 
 interface IDecodedUser {
-  user_id: string,
-  iat: number
+  user_id: string;
+  iat: number;
 }
 
-const auth=handler(async (req, res, next)=>{
-  const reqToken = req.header("Authorization") as string;
-  const decodedUser = jwt.verify(reqToken, process.env.JWT_TOKEN_KEY as string) as IDecodedUser;
+const auth = handler(async (req, res, next) => {
+  try {
+    const reqToken = req.header('Authorization') as string;
 
-  next()
+    const decodedUser = jwt.verify(reqToken, process.env.JWT_TOKEN_KEY as string) as IDecodedUser;
+    const user = await User.findById(decodedUser.user_id);
 
-  const user = await User.findById(decodedUser.user_id);
+    req.user = user;
+    req.currentToken = reqToken;
 
-  if (!user) throw new AuthenticationError()
-
-  const userHasToken = user.tokens.some(({ token }: any) => token == reqToken);
-  if (!userHasToken) throw Error('Wrong user id');
-
-  req.user = user;
-  req.currentToken = reqToken;
-
-  next();
-})
+    next();
+  } catch (err) {
+    throw new AuthenticationError();
+  }
+});
 
 export default auth;

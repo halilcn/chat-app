0e0fcdab4fc10c3807a49cc5df89a08657cee1c5
halilcn <halilc.2001@gmail.com>
2022-03-25 21:16:57 +0300

    added permission for friend message

---
+++ backend/src/middlewares/permissions/friend-message-permission.ts
@@ -0,0 +1,11 @@
+import handler from '@shared/handler';
+import Friend from '@models/friend-model';
+import { ForbiddenError } from '@shared/errors';
+
+export default handler(async (req, res, next) => {
+  const friend = await Friend.findOne({ _id: req.params.friendId });
+
+  if (friend.requester != req.user._id && friend.recipient != req.user._id) throw new ForbiddenError();
+
+  next();
+});
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,11 +4,12 @@ import userActionsRouter from '@routes/user/user-action-router';
 import friendRouter from '@routes/friend/friend-router';
 import messageRouter from '@routes/message/message-router';
 import auth from '@middlewares/auth';
+import friendMessagePermission from '@middlewares/permissions/friend-message-permission';
 
 const router = Router();
 
 router.use('/v1/user-action/', userActionsRouter);
 router.use('/v1/friends/', auth, friendRouter);
-router.use('/v1/friends/:friendId/messages/', auth, messageRouter);
+router.use('/v1/friends/:friendId/messages/', auth, friendMessagePermission, messageRouter);
 
 export default router;

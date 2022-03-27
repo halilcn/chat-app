90c378a8c73b19d4d85733f14e0da1b2541daa05
halilcn <halilc.2001@gmail.com>
2022-03-27 23:16:03 +0300

    added message send content

--- frontend/src/components/dashboard/main/MainContentWrapper.vue
+++ frontend/src/components/dashboard/main/MainContentWrapper.vue
@@ -2,18 +2,21 @@
   <div class="main-content-container">
     <top-content class="content" />
     <message-content class="content" />
+    <send-message-content />
   </div>
 </template>
 
 <script>
 import TopContent from '@/components/dashboard/main/TopContent';
 import MessageContent from '@/components/dashboard/main/MessageContent';
+import SendMessageContent from '@/components/dashboard/main/SendMessageContent';
 
 export default {
   name: 'MainContent',
   components: {
     TopContent,
-    MessageContent
+    MessageContent,
+    SendMessageContent
   }
 };
 </script>
@@ -22,6 +25,8 @@ export default {
 .main-content-container {
   width: 100%;
   background-color: $default-bg-blue-color;
+  display: flex;
+  flex-direction: column;
 
   .content {
     padding: 20px;
---
+++ frontend/src/components/dashboard/main/SendMessageContent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="send-message-content-container">
+    <input class="message" type="text" value="asdasd asdkasd jalsdjaks dkd" />
+    <div class="actions">
+      <i class="fa-solid fa-paperclip item"></i>
+      <div class="send-message-button item">
+        <i class="fa-solid fa-paper-plane"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SendMessageContent'
+};
+</script>
+
+<style lang="scss" scoped>
+.send-message-content-container {
+  margin: auto 20px 20px 20px;
+  background-color: white;
+  padding: 20px;
+  border-radius: 100px;
+  display: flex;
+  justify-content: space-between;
+
+  .message {
+    width: 100%;
+    border: 0;
+  }
+
+  .actions {
+    display: flex;
+
+    .item {
+      margin-left: 10px;
+    }
+  }
+}
+</style>

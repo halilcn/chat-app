9919b630eb75e1d33aa556963d5b23512ca6b168
halilcn <halilc.2001@gmail.com>
2022-03-29 21:16:34 +0300

    worked on send message

--- frontend/src/components/dashboard/main/SendMessageContent.vue
+++ frontend/src/components/dashboard/main/SendMessageContent.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="send-message-content-container">
-    <input class="message" type="text" value="asdasd asdkasd jalsdjaks dkd" />
+    <input class="message" type="text"  placeholder="Your message..."/>
     <div class="actions">
-      <i class="fa-solid fa-paperclip item"></i>
+      <i class="fa-solid fa-face-smile item icons"></i>
+      <i class="fa-solid fa-paperclip item file"></i>
       <div class="send-message-button item">
         <i class="fa-solid fa-paper-plane"></i>
       </div>
@@ -27,14 +28,27 @@ export default {
 
   .message {
     width: 100%;
+    max-height: 300px;
     border: 0;
+    overflow-y: auto;
+    color: $blue-dark;
+
+    &::placeholder{
+      color: $blue-light-dark;
+    }
   }
 
   .actions {
     display: flex;
+    align-items: center;
+    font-size: 20px;
 
     .item {
-      margin-left: 10px;
+      margin-left: 15px;
+
+      &.send-message-button{
+        color: $default-purple;
+      }
     }
   }
 }

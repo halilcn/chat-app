fc98aaef98de342d29729dfeb3dd874acfd21be9
halilcn <halilc.2001@gmail.com>
2022-04-02 23:34:45 +0300

    fixed auth module

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -7,7 +7,7 @@ import friend from '@/store/modules/friend';
 import file from '@/store/modules/file';
 
 axios.defaults.baseURL = process.env.VUE_APP_BACKEND_BASE_URL;
-axios.defaults.headers.common['Authorization'] = auth.state.user?.token;
+axios.defaults.headers.common['Authorization'] = auth.state.user ? auth.state.user.token : '';
 
 export default createStore({
   modules: {

6abed8f5bc9e841da349d1d4005ceba0de3989e1
halilcn <halilc.2001@gmail.com>
2022-04-07 14:39:39 +0300

    added dynamic user list

--- frontend/src/components/dashboard/side/user/UserList.vue
+++ frontend/src/components/dashboard/side/user/UserList.vue
@@ -1,107 +1,114 @@
 <template>
   <div class="user-list-container">
-    <div class="list">
-      <div class="item not-readed">
-        <img class="image" src="https://randomuser.me/api/portraits/men/40.jpg" />
+    <div v-if="userListMessages.length > 0" class="list">
+      <div v-for="(message, index) in userListMessages" :key="index" class="item selected not-readed">
+        <img class="image" :src="message.user.image" />
         <div class="chat-info">
-          <div class="name">Test İsim</div>
-          <div class="last-message not-readed">Bu son mesajds adasd asdsa asda ...</div>
+          <div class="name">{{ message.user.nameSurname }}</div>
+          <div class="last-message" :class="{ 'not-readed': userHasUnreadMessages(message.unReadMessagesCount) }">
+            {{ message.lastMessage.content }}
+          </div>
         </div>
         <div class="other-info">
-          <div class="time">12:52</div>
-          <div v-if="false" class="total-unread-message">4</div>
-        </div>
-      </div>
-      <div class="item selected">
-        <img class="image" src="https://randomuser.me/api/portraits/men/40.jpg" />
-        <div class="chat-info">
-          <div class="name">Test İsim</div>
-          <div class="last-message">Bu son mesajds adasd asdsa asda ...</div>
-        </div>
-        <div class="other-info">
-          <div class="time">12:52</div>
-          <div v-if="false" class="total-unread-message">4</div>
-        </div>
-      </div>
-      <div class="item">
-        <img class="image" src="https://randomuser.me/api/portraits/men/40.jpg" />
-        <div class="chat-info">
-          <div class="name">Test İsim</div>
-          <div class="last-message">Bu son mesajds adasd asdsa asda ...</div>
-        </div>
-        <div class="other-info">
-          <div class="time">12:52</div>
-          <div v-if="false" class="total-unread-message">4</div>
+          <div class="time">{{ this.$dayjs(message.lastMessage.createdAt).format('hh:mm') }}</div>
+          <div v-if="userHasUnreadMessages(message.unReadMessagesCount)" class="total-unread-message">
+            {{ message.unReadMessagesCount }}
+          </div>
         </div>
       </div>
     </div>
+    <div v-else class="no-chat">No chat</div>
   </div>
 </template>
 
 <script>
+import * as dayjs from 'dayjs';
+import { mapActions, mapState } from 'vuex';
+
 export default {
-  name: 'UserList'
+  name: 'UserList',
+  methods: {
+    ...mapActions('message', ['getUserListMessages']),
+    userHasUnreadMessages(count) {
+      return count > 0;
+    }
+  },
+  created() {
+    this.getUserListMessages();
+  },
+  computed: {
+    ...mapState('message', ['userListMessages'])
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.list {
-  .item {
-    display: flex;
-    padding: 20px;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: 0.2s;
+.user-list-container {
+  .no-chat {
+    margin-top: 20px;
+    text-align: center;
+    font-size: 18px;
+    color: #8e8e8e;
+  }
 
-    &:hover {
-      background-color: $light-grey-color;
-    }
-
-    &.selected {
-      background-color: #f4f9ff;
-    }
-
-    &:not(.selected).not-readed {
-      .last-message,
-      .time {
-        font-weight: 600;
-        color: $blue-dark !important;
-      }
-    }
-
-    .image {
-      width: 50px;
-      height: 50px;
-      border-radius: 100%;
-    }
-
-    .chat-info {
-      width: 100%;
+  .list {
+    .item {
       display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      margin-left: 20px;
+      padding: 20px;
+      border-radius: 10px;
+      cursor: pointer;
+      transition: 0.2s;
 
-      .name {
-        font-size: 17px;
-        font-weight: 500;
+      &:hover {
+        background-color: $light-grey-color;
       }
 
-      .last-message {
-        font-size: 14px;
-        color: $blue-light-dark;
+      &.selected {
+        background-color: #f4f9ff;
       }
-    }
 
-    .other-info {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: flex-end;
+      &:not(.selected).not-readed {
+        .last-message,
+        .time {
+          font-weight: 600;
+          color: $blue-dark !important;
+        }
+      }
 
-      .time {
-        font-size: 13px;
-        color: $blue-light-dark;
+      .image {
+        width: 50px;
+        height: 50px;
+        border-radius: 100%;
+      }
+
+      .chat-info {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        margin-left: 20px;
+
+        .name {
+          font-size: 17px;
+          font-weight: 500;
+        }
+
+        .last-message {
+          font-size: 14px;
+          color: $blue-light-dark;
+        }
+      }
+
+      .other-info {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-end;
+
+        .time {
+          font-size: 13px;
+          color: $blue-light-dark;
+        }
       }
     }
   }
--- frontend/src/store/modules/message.ts
+++ frontend/src/store/modules/message.ts
@@ -9,14 +9,23 @@ interface CustomObject {
 export default {
   state: {
     messages: [],
+    userListMessages: [],
     selectedUserId: null
   },
   mutations: {
     setSelectedUserId(state: CustomObject, payload: string) {
       state.selectedUserId = payload;
+    },
+    setUserListMessages(state: CustomObject, payload: Array<object>) {
+      state.userListMessages = payload;
     }
   },
-  actions: {},
+  actions: {
+    async getUserListMessages({ commit }: { commit: Commit }) {
+      const { data } = (await axios.get('/user-messages')).data;
+      commit('setUserListMessages', data.messages);
+    }
+  },
   getters: {},
   namespaced: true
 };

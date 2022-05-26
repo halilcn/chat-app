1629954e4f90ccf1035686b4e87a5f0bbef26349
halilcn <halilc.2001@gmail.com>
2022-05-26 22:40:10 +0300

    add tests for user actions

--- backend/__tests__/api/user/user-action.test.ts
+++ backend/__tests__/api/user/user-action.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { faker } from '@faker-js/faker';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import server from '../../../src/server';
 import clearDb from '../../utils/clear-db';
@@ -31,5 +32,20 @@ describe('User Action', () => {
     await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
 
     await request(server).post('/api/v1/user-action/login').send({ username: user.username, password: user.password }).expect(201);
+  });
+
+  it('should return 200 - /logout', async () => {
+    const user = {
+      username: faker.internet.userName(),
+      nameSurname: faker.name.firstName(),
+      password: faker.internet.password()
+    };
+
+    const createdUser = await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
+    const token = jwt.sign({ user_id: createdUser._id }, process.env.JWT_TOKEN_KEY as string);
+
+    await User.findOneAndUpdate({ _id: createdUser._id }, { tokens: [{ token }] });
+
+    await request(server).post('/api/v1/user-action/logout').set('Authorization', token).expect(200);
   });
 });
---
+++ backend/__tests__/utils/clear-db.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+export default async () => {
+/*  const collections = await mongoose.connection.db.collections();
+  for (const collection of collections) {
+    // @ts-ignore
+    await collection.remove();
+  }*/
+};

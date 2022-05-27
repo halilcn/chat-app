e9029a28f660572bc4f116cd0c5870dceeba06db
halilcn <halilc.2001@gmail.com>
2022-05-28 00:46:12 +0300

    add new tests

---
+++ backend/__tests__/middlewares/auth.test.ts
@@ -0,0 +1,68 @@
+import { NextFunction, Request, Response } from 'express';
+
+import auth from '@middlewares/auth';
+import { AuthenticationError } from '@shared/errors';
+import CustomError from '@shared/errors/custom-error';
+import { faker } from '@faker-js/faker';
+import jwt from 'jsonwebtoken';
+import User from '@models/user-model';
+import bcrypt from 'bcrypt';
+import connectDb from '../utils/connect-db';
+
+describe('Auth Middleware', () => {
+  let mockRequest: Partial<Request> = {};
+  const mockResponse: Partial<Response> = {};
+  const nextFunction: Partial<NextFunction> = jest.fn();
+
+  beforeAll(() => {
+    connectDb();
+  });
+
+  beforeEach(() => {
+    mockRequest = {};
+  });
+
+  describe('should not be user', () => {
+    test('without authorization header', async () => {
+      await auth(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
+
+      expect(mockRequest.user).toEqual(undefined);
+    });
+
+    test('with wrong authorization header', async () => {
+      mockRequest = {
+        headers: {
+          Authorization: 'test-token'
+        }
+      };
+
+      await auth(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
+
+      expect(mockRequest.user).toEqual(undefined);
+    });
+  });
+
+  //todo:!!
+  test('should be user with authorization header', async () => {
+    /*const user = {
+      username: faker.internet.userName(),
+      nameSurname: faker.name.firstName(),
+      password: faker.internet.password()
+    };
+    const createdUser = await User.create(user);
+    const token = jwt.sign({ user_id: createdUser._id }, process.env.JWT_TOKEN_KEY as string);
+    await User.findOneAndUpdate({ _id: createdUser._id }, { tokens: [{ token }] });
+    mockRequest = {
+      headers: {
+        Authorization: token
+      }
+    };
+
+    await auth(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    console.log('tests asdadsa dasdsa dasdasad sa');
+    console.log(mockRequest.user);*/
+
+    expect(mockRequest.user).toEqual(undefined);
+  });
+});
---
+++ backend/__tests__/utils/connect-db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+import logger from '@shared/logger';
+
+require('dotenv').config();
+
+export default () => {
+  mongoose
+    .connect(process.env.MONGO_DB_URI as string)
+    .then(() => {
+      logger.info('Connected to mongoDB');
+    })
+    .catch((err: Error) => {
+      logger.error(`Mongoose connect error:${err.message}`);
+      process.exit(1);
+    });
+};

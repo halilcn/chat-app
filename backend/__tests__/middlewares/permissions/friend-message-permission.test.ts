ca028f7c92b3944617e53b1720d767814057af12
halilcn <halilc.2001@gmail.com>
2022-05-28 13:22:58 +0300

    add test in permission

--- backend/__tests__/middlewares/permissions/friend-message-permission.test.ts
+++ backend/__tests__/middlewares/permissions/friend-message-permission.test.ts
@@ -1,12 +1,13 @@
 import { faker } from '@faker-js/faker';
-import User from '@models/user-model';
 import jwt from 'jsonwebtoken';
-import auth from '@middlewares/auth';
 import { NextFunction, Request, Response } from 'express';
-import connectDb from '../../test-utils/connect-db';
-import clearDb from '../../test-utils/clear-db';
+
+import User from '@models/user-model';
+import auth from '@middlewares/auth';
 import Friend from '@models/friend-model';
 import friendMessagePermission from '@middlewares/permissions/friend-message-permission';
+import connectDb from '../../test-utils/connect-db';
+import clearDb from '../../test-utils/clear-db';
 
 describe('Friend Message Permission Middleware', () => {
   let mockRequest: Partial<Request> = {};
@@ -19,6 +20,7 @@ describe('Friend Message Permission Middleware', () => {
 
   beforeEach(async () => {
     mockRequest = {};
+    nextFunction = jest.fn();
     await clearDb();
   });
 
@@ -52,12 +54,43 @@ describe('Friend Message Permission Middleware', () => {
       friendId: createdFriendBetweenUsers._id
     };
 
-    nextFunction = ({ status }: { status: number }) => {
-      if (status === 403) hasErrorCode = true;
+    nextFunction = (error: any) => {
+      if (error && error?.status === 400) hasErrorCode = true;
     };
 
     await friendMessagePermission(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
 
     expect(hasErrorCode).toEqual(false);
+  });
+
+  test('should be error with wrong friend id', async () => {
+    let hasErrorCode = false;
+    const user = {
+      username: faker.internet.userName(),
+      nameSurname: faker.name.firstName(),
+      password: faker.internet.password()
+    };
+
+    const createdUser = await User.create(user);
+    const token = jwt.sign({ user_id: createdUser._id }, process.env.JWT_TOKEN_KEY as string);
+    await User.findOneAndUpdate({ _id: createdUser._id }, { tokens: [{ token }] });
+    mockRequest = {
+      headers: {
+        Authorization: token
+      }
+    };
+    await auth(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
+
+    mockRequest.params = {
+      friendId: faker.datatype.uuid()
+    };
+
+    nextFunction = ({ status }: { status: number }) => {
+      if (status === 400) hasErrorCode = true;
+    };
+
+    await friendMessagePermission(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
+
+    expect(hasErrorCode).toEqual(true);
   });
 });

a6ac51dc5eba8780bdf1f695dd58f41b047165ad
halilcn <halilc.2001@gmail.com>
2022-05-31 23:27:38 +0300

    add new tests for friend route

--- backend/__tests__/api/friend/friend.test.ts
+++ backend/__tests__/api/friend/friend.test.ts
@@ -1,13 +1,13 @@
-import clearDb from '../../test-utils/clear-db';
 import { faker } from '@faker-js/faker';
-import User from '@models/user-model';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import Friend from '@models/friend-model';
 import request from 'supertest';
-import server from '../../../src/server';
 
-//todo:test name
+import User from '@models/user-model';
+import Friend from '@models/friend-model';
+import server from '../../../src/server';
+import clearDb from '../../test-utils/clear-db';
+
 describe('Friend', () => {
   beforeEach(async () => {
     await clearDb();
@@ -62,7 +62,6 @@ describe('Friend', () => {
     });
   });
 
-  //todo: gerçekten kaydetmiş mi
   describe('POST - /v1/friends', () => {
     it('should return 201 with correct friend user id', async function () {
       const user = {
@@ -119,10 +118,34 @@ describe('Friend', () => {
 
       await request(server).post('/api/v1/friends').set('Authorization', token).expect(422);
     });
+
+    it('length of friends should be 1 after request to create friend', async function () {
+      const user = {
+        username: faker.internet.userName() + '-test-user',
+        nameSurname: faker.name.firstName(),
+        password: faker.internet.password()
+      };
+      const friendUser = {
+        username: faker.internet.userName() + '-test-friend',
+        nameSurname: faker.name.firstName(),
+        password: faker.internet.password()
+      };
+      const createdUser = await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
+      const createdFriendUser = await User.create(friendUser);
+
+      const token = jwt.sign({ user_id: createdUser._id }, process.env.JWT_TOKEN_KEY as string);
+      await User.findOneAndUpdate({ _id: createdUser._id }, { tokens: [{ token }] });
+
+      await request(server).post('/api/v1/friends').set('Authorization', token).send({ recipient: createdFriendUser._id });
+
+      const lengthFriends = (await Friend.find().or([{ requester: createdUser._id }, { recipient: createdUser._id }])).length;
+
+      expect(lengthFriends).toEqual(1);
+    });
   });
 
   describe('GET - /v1/friends/:friendId', () => {
-    it('should return 200', async function () {
+    it('should return 200 after create a friend', async function () {
       const user = {
         username: faker.internet.userName() + '-test-user',
         nameSurname: faker.name.firstName(),
@@ -169,7 +192,6 @@ describe('Friend', () => {
     });
   });
 
-  //todo: gerçekten Silmiş mi?
   describe('DELETE - /v1/friends/:friendId', () => {
     it('should return 200', async function () {
       const user = {
@@ -192,11 +214,36 @@ describe('Friend', () => {
 
       await request(server).delete(`/api/v1/friends/${friend._id}`).set('Authorization', token).expect(200);
     });
+
+    it('length of friends should be 0 after request to delete friend', async function () {
+      const user = {
+        username: faker.internet.userName() + '-test-user',
+        nameSurname: faker.name.firstName(),
+        password: faker.internet.password()
+      };
+      const friendUser = {
+        username: faker.internet.userName() + '-test-friend',
+        nameSurname: faker.name.firstName(),
+        password: faker.internet.password()
+      };
+      const createdUser = await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
+      const createdFriendUser = await User.create(friendUser);
+
+      const token = jwt.sign({ user_id: createdUser._id }, process.env.JWT_TOKEN_KEY as string);
+      await User.findOneAndUpdate({ _id: createdUser._id }, { tokens: [{ token }] });
+
+      const friend = await Friend.create({ requester: createdUser._id, recipient: createdFriendUser._id });
+
+      await request(server).delete(`/api/v1/friends/${friend._id}`).set('Authorization', token).expect(200);
+
+      const lengthFriends = (await Friend.find().or([{ requester: createdUser._id }, { recipient: createdUser._id }])).length;
+
+      expect(lengthFriends).toEqual(0);
+    });
   });
 
-  describe('get - /v1/friends/search', () => {
-    it('should return 200', async function () {
-      //todo:!
+  describe('GET - /v1/friends/search', () => {
+    it('should return 200 after create a friend', async function () {
       const user = {
         username: faker.internet.userName() + '-test-user',
         nameSurname: faker.name.firstName(),
@@ -215,7 +262,31 @@ describe('Friend', () => {
 
       await Friend.create({ requester: createdUser._id, recipient: createdFriendUser._id });
 
-      await request(server).delete(`/api/v1/friends/search?value=${friendUser.username}`).set('Authorization', token).expect(200);
+      await request(server).get(`/api/v1/friends/search?value=${friendUser.username}`).set('Authorization', token).expect(200);
+    });
+
+    it('length of response"s users should be 1 when request to get users by username', async function () {
+      const user = {
+        username: faker.internet.userName() + '-test-user',
+        nameSurname: faker.name.firstName(),
+        password: faker.internet.password()
+      };
+      const friendUser = {
+        username: faker.internet.userName() + '-test-friend',
+        nameSurname: faker.name.firstName(),
+        password: faker.internet.password()
+      };
+      const createdUser = await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
+      const createdFriendUser = await User.create(friendUser);
+
+      const token = jwt.sign({ user_id: createdUser._id }, process.env.JWT_TOKEN_KEY as string);
+      await User.findOneAndUpdate({ _id: createdUser._id }, { tokens: [{ token }] });
+
+      await Friend.create({ requester: createdUser._id, recipient: createdFriendUser._id });
+
+      const res = await request(server).get(`/api/v1/friends/search?value=${friendUser.username}`).set('Authorization', token);
+
+      expect(res.body.data.users.length).toEqual(1);
     });
   });
 });

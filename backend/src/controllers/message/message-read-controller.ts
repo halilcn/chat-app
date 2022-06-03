e3f8a1ae687e871bc35d01daf334f88e99fd3bbe
halilcn <halilc.2001@gmail.com>
2022-06-03 22:58:06 +0300

    fix message read respoonse

--- backend/src/controllers/message/message-read-controller.ts
+++ backend/src/controllers/message/message-read-controller.ts
@@ -2,9 +2,12 @@ import { ObjectId } from 'mongoose';
 
 import handler from '@shared/handler';
 import MessageService from '@services/message-service';
+import response from '@shared/response';
 
 const store = handler(async (req, res, next) => {
   await MessageService.updateAsReadMessages(req.params.friendId as unknown as ObjectId, req.user._id, req.validated.messageIds);
+
+  next(response.success());
 });
 
 export default {

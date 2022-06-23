0a97b66d3331b4c9a1169cd258e8380d9df487e3
halilcn <halilc.2001@gmail.com>
2022-06-23 15:33:54 +0300

    add new test for loading dashboard

---
+++ frontend/tests/unit/src/others/LoadingDashboard.ts
@@ -0,0 +1,18 @@
+import { VueWrapper } from '@vue/test-utils';
+
+import LoadingDashboard from '@/components/dashboard/others/LoadingDashboard.vue';
+import renderComponent from '../../test-utils/render-component';
+
+describe('UserDashboard', function () {
+  let wrapper: VueWrapper;
+
+  const refreshComponent = () => {
+    wrapper = renderComponent(LoadingDashboard);
+  };
+
+  it('should render component without crashing', () => {
+    refreshComponent();
+
+    expect(wrapper).toBeTruthy();
+  });
+});
